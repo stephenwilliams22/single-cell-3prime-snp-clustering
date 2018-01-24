6fa33f61c1c2eadaa4384be5b737e3f798a0aca5
Stephen Williams <stephen.williams@10xgenomics.com>
2018-01-24 10:26:10 -0800

    Update __init__.py

--- mro/stages/snpclust/call_snps/__init__.py
+++ mro/stages/snpclust/call_snps/__init__.py
@@ -16,64 +16,29 @@ stage CALL_SNPS(
     in  int    n_donors,
     out vcf[]  output,
     src py     "stages/snpclust/call_snps_pd",
-    in  path    bed_file,
 ) split using (
     in  string locus,
 )
 '''
 
 def split(args):
-    #in_bam = tk_bam.create_bam_infile(args.input)
-    #define a specific region to investigate
-    #loci = ["1:0..40000000"]
-    #loci = tk_bam.generate_tiling_windows(in_bam, tk_constants.PARALLEL_LOCUS_SIZE)
-    loci = [x.split() for x in open(args.bed_file)]
+    in_bam = tk_bam.create_bam_infile(args.input)
+    loci = tk_bam.generate_tiling_windows(in_bam, tk_constants.PARALLEL_LOCUS_SIZE)
     chunks = [{'locus': locus} for locus in loci]
     return {'chunks': chunks}
 
 def main(args, outs):
     genome_fasta_path = cr_utils.get_reference_genome_fasta(args.reference_path)
-    
-    #chrom, start, stop = tk_io.get_locus_info(args.locus)
-    chrom, start, stop = args.locus
+
+    chrom, start, stop = tk_io.get_locus_info(args.locus)
     bed_path = martian.make_path('region.bed')
     with open(bed_path, 'w') as f:
         f.write(chrom+"\t"+str(start)+"\t"+str(stop)+"\n")
-    
-    dic_make_args = ['gatk-launch', 'CreateSequenceDictionary', '-R', genome_fasta_path]
-    subprocess.call(dic_make_args)
-    
-    
-    first_bam = martian.make_path('output.RG.bam')
-    second_bam = martian.make_path('output.RG.STARcor.bam')
-    rg_make_args = ['gatk-launch', 'AddOrReplaceReadGroups', '-I', args.input, '-O', 
-                    first_bam, '-LB', 'lib1', '-PL', 'illumina',
-                    '-PU', 'unit1', '-SM', 'example']
-    subprocess.check_call(rg_make_args)
-    
-    #this corrects the STAR mapq annotation. Uses 8 threads.
-    samtools_args = '''samtools view -@ 8 -h {} | 
-                       awk 'BEGIN{{OFS="\t"}} $5 == 255 {{ $5 = 60; print; next}} {{print}}' | 
-                       samtools view -Sb -@ 8 - > {}'''.format(first_bam, second_bam)
-    subprocess.call(samtools_args, shell=True)            
-    
-    samtools_index_args = ['samtools', 'index',second_bam]
-    subprocess.call(samtools_index_args)
-    
-    gatk_args = ['gatk-launch', 'HaplotypeCaller', '-R', genome_fasta_path, '-I', second_bam, 
-                 '--minimum-mapping-quality', '30', '--min-base-quality-score', '20', '-L', bed_path, '-O','output.vcf']
-    
-    
-    
-    #os.remove first_bam
-    
+
+    freebayes_args = ['freebayes', '-f', genome_fasta_path, '-b', args.input, '-0', '-t', bed_path]
+
     with open(outs.output, 'w') as f:
-        subprocess.call(gatk_args, stdout=f)
-    
-    #modify the GATK vcf to remove header issues
-    sed_args = '''sed -i '/##FORMAT=<ID=PL/,/##INFO=<ID=AC/{//!d}' output.vcf'''
-    subprocess.call(sed_args, shell=True)
-    
+        subprocess.check_call(freebayes_args, stdout=f)
+
 def join(args, outs, chunk_defs, chunk_outs):
     outs.output = [chunk.output for chunk in chunk_outs]
-   
