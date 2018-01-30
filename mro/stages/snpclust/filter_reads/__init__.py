ea3b4c2ea9fb1680ddeefa09c03f27e9d29e33fe
Stephen Williams <stephen.williams@10xgenomics.com>
2018-01-29 16:35:20 -0800

    Update __init__.py

--- mro/stages/snpclust/filter_reads/__init__.py
+++ mro/stages/snpclust/filter_reads/__init__.py
@@ -9,7 +9,7 @@ import cellranger.utils as cr_utils
 
 __MRO__ = '''
 stage FILTER_READS(
-    in  bam    input,
+    in  bam    second_bam,
     in  tsv    cell_barcodes,
     in  map    align,
     out bam    output,
