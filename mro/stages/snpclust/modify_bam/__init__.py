3a37c8afb6b997e376845372bc97017d438c0b9f
Stephen Williams <stephen.williams@10xgenomics.com>
2018-01-31 23:34:58 -0800

    Update __init__.py
    adding bam mods for mutect2

--- mro/stages/snpclust/modify_bam/__init__.py
+++ mro/stages/snpclust/modify_bam/__init__.py
@@ -24,21 +24,44 @@ def main(args, outs):
     #dic_make_args = ['gatk-launch', 'CreateSequenceDictionary', '-R', genome_fasta_path]
     #subprocess.check_call(dic_make_args)
     
-    first_bam = martian.make_path('output.RG.bam')
-    second_bam = martian.make_path('output.RG.STARcor.bam')
-    rg_make_args = ['gatk-launch', 'AddOrReplaceReadGroups', '-I', args.input, '-O', 
-                      first_bam, '-LB', 'lib1', '-PL', 'illumina','-PU', 'unit1', '-SM', 'sample']
-    subprocess.check_call(rg_make_args)
+    #add readgroups to the bams
+    first_tumor_bam = martian.make_path('output.tumor.RG.bam')
+    second_tumor_bam = martian.make_path('output.tumor.RG.STARcor.bam')
+    rg_make_tumor_args = ['gatk-launch', 'AddOrReplaceReadGroups', '-I', args.tumor.input, '-O', 
+                      first_tumor_bam, '-LB', 'lib1', '-PL', 'illumina','-PU', 'unit1', '-SM', 'sample']
+    subprocess.check_call(rg_make_tumor_args)
+    
+    
+    first_normal_bam = martian.make_path('output.normal.RG.bam')
+    second_normal_bam = martian.make_path('output.normal.RG.STARcor.bam')
+    rg_make_normal_args = ['gatk-launch', 'AddOrReplaceReadGroups', '-I', args.input, '-O', 
+                      first_normal_bam, '-LB', 'lib1', '-PL', 'illumina','-PU', 'unit1', '-SM', 'sample']
+    subprocess.check_call(rg_make_normal_args)
       
       
     #this corrects the STAR mapq annotation. Uses 8 threads.
-    samtools_args = '''samtools view -@ 8 -h {} |
+    samtools_tumor_args = '''samtools view -@ 8 -h {} |
       awk 'BEGIN{{OFS="\t"}} $5 == 255 {{ $5 = 60; print; next}} {{print}}' | 
-      samtools view -Sb -@ 8 - > {}'''.format(first_bam, second_bam)
+      samtools view -Sb -@ 8 - > {}'''.format(first_tumor_bam, second_tumor_bam)
     
-    subprocess.check_call(samtools_args, shell=True)            
+    subprocess.check_call(samtools_tumor_args, shell=True)
     
-    os.remove(first_bam)
-      
-    samtools_index_args = ['samtools', 'index',second_bam]
-    subprocess.check_call(samtools_index_args)
+    #this corrects the STAR mapq annotation. Uses 8 threads.
+    samtools_normal_args = '''samtools view -@ 8 -h {} |
+      awk 'BEGIN{{OFS="\t"}} $5 == 255 {{ $5 = 60; print; next}} {{print}}' | 
+      samtools view -Sb -@ 8 - > {}'''.format(first_normal_bam, second_normal_bam)
+    
+    subprocess.check_call(samtools_normal_args, shell=True)   
+    
+    #clean up
+    os.remove(first_tumor_bam)
+    os.remove(first_normal_bam)
+
+    #index  
+    samtools_index_tumor_args = ['samtools', 'index',second_tumor_bam]
+    subprocess.check_call(samtools_index_tumor_args)
+    
+    #index  
+    samtools_index_normal_args = ['samtools', 'index',second_normal_bam]
+    subprocess.check_call(samtools_index_normal_args)
+
