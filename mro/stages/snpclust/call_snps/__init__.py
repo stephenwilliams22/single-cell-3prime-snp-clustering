920a40b23abefb2e796e01c6b62ca88fe31aed26
Stephen Williams <stephen.williams@10xgenomics.com>
2018-02-24 15:59:32 -0800

    Update __init__.py
    took out max reads in memmory. need to put it in SplitNCigars in Modify_bam

--- mro/stages/snpclust/call_snps/__init__.py
+++ mro/stages/snpclust/call_snps/__init__.py
@@ -47,8 +47,7 @@ def main(args, outs):
                  '--minimum-mapping-quality', '30', 
                  '--min-base-quality-score', '20', 
                  '--dont-use-soft-clipped-bases', 'true', 
-                 '--add-output-vcf-command-line', 'false',
-                 '--max-reads-in-memory', '50000']
+                 '--add-output-vcf-command-line', 'false']
             
     subprocess.check_call(gatk_args)
         
