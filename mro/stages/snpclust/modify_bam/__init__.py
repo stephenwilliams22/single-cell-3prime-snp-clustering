c7009a404b9124f3b487e2fa3e65bac746d4d355
Stephen Williams <stephen.williams@10xgenomics.com>
2018-02-26 13:27:12 -0800

    Update __init__.py

--- mro/stages/snpclust/modify_bam/__init__.py
+++ mro/stages/snpclust/modify_bam/__init__.py
@@ -65,8 +65,8 @@ def main(args, outs):
     subprocess.check_call(star_args)
     
     #sort and index the bam
-    args = ['samtools', 'sort', 'output.bam', '>', 'output_sorted.bam']
-    subprocess.check_call(args)
+    args = ['samtools', 'sort', output_bam, '-o', 'output_sorted.bam']
+    subprocess.check_call(args, shell=True)
     #tk_bam.sort(output_bam)
     #os.remove(outs.output)
     os.rename('output_sorted.bam', 'output.bam')
